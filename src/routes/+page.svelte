d3b27e504b90d5ec59e6def3d7707682de608af2
ZachNiederstadt <niederstadtz@gmail.com>
2024-10-18 14:24:54 -0500

    home page more

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,46 +11,50 @@
 
 <!-- whole page background -->
 <div
-  class="flex items-center justify-center absolute inset-0 bg-gradient-to-r from-transparent via-blue-400 to-blue-950 bg-[length:1200%_1200%] animate-gradient-animate z-[-1] h-dvh"
-  transition:fade={{ duration: 250 }}
+  class="bg-cover bg-center"
+  style="background-image: url('/images/welcome.avif');"
 >
-  <!-- contents welcome etc...does not need a height -->
   <div
-    class="m-auto flex px-4 py-12 bg-opacity-50"
+    class="flex items-center justify-center inset-0 bg-gradient-to-r from-transparent via-blue-950 bg-[length:1200%_1200%] animate-gradient-animate z-[-1] h-dvh"
     transition:fade={{ duration: 250 }}
   >
-    <div class="text-center max-w-3xl w-full">
-      <h1 class="text-4xl sm:text-5xl font-extrabold text-white mb-0">
-        Welcome!
-      </h1>
+    <!-- contents welcome etc...does not need a height -->
+    <div
+      class="m-auto flex px-4 py-12 bg-opacity-50"
+      transition:fade={{ duration: 250 }}
+    >
+      <div class="text-center max-w-3xl w-full">
+        <h1 class="text-4xl sm:text-5xl font-extrabold text-white mb-0">
+          Welcome!
+        </h1>
 
-      <p
-        class="text-base sm:text-lg text-white mb-8 sm:mb-7 pt-5"
-        transition:fade={{ duration: 250 }}
-      >
-        Check out my work
-      </p>
-      <div class="pt-5" out:slide>
-        <a
-          class="btn btn-primary btn-lg mt-4 px-8 py-2 text-white font-orbitron text-sm tracking-widest font-semibold rounded-full bg-blue-600 hover:bg-blue-400 transition duration-300 ease-in-out"
-          style=""
-          href="photography"
-          target="_blank"
+        <p
+          class="text-base sm:text-lg text-white mb-8 sm:mb-7 pt-5 pb-20"
+          transition:fade={{ duration: 250 }}
         >
-          Photography
-        </a>
-      </div>
-      <div class="pt-7" out:slide>
-        <a
-          class="btn btn-primary btn-lg mt-4 px-8 py-2 text-white font-orbitron text-sm tracking-widest font-semibold rounded-full bg-blue-600 hover:bg-blue-400 transition duration-300 ease-in-out"
-          style="color: white"
-          href="/socials"
-          target="_blank"
-        >
-          Socials
-        </a>
-      </div>
-      <!-- <a
+          Check out my work
+        </p>
+        <div class="pt-20" out:slide>
+          <a
+            class="btn mt-5 px-7 py-3 text-white font-orbitron text-sm tracking-widest font-semibold rounded-full bg-blue-600 hover:bg-blue-400 transition duration-300 ease-in-out"
+            style=""
+            href="photography"
+            target="_blank"
+          >
+            Photography
+          </a>
+        </div>
+        <div class="pt-8" out:slide>
+          <a
+            class="btn mt-5 px-8 py-3 text-white font-orbitron text-sm tracking-widest font-semibold rounded-full bg-blue-600 hover:bg-blue-400 transition duration-300 ease-in-out"
+            style="color: white"
+            href="/socials"
+            target="_blank"
+          >
+            Socials
+          </a>
+        </div>
+        <!-- <a
           href="https://github.com/ZachNiederstadt"
           target="_blank"
           class="bg-blue-600 text-white font-semibold py-3 px-6 rounded-lg shadow-md hover:bg-blue-400 transition duration-300 hover:text-blue-500 w-[110px] h-[40px] flex justify-center items-center"
@@ -66,6 +70,7 @@
         >
           Resume
         </a> -->
+      </div>
     </div>
   </div>
 </div>
@@ -85,6 +90,6 @@
   }
 
   .animate-gradient-animate {
-    animation: gradientAnimation 15s ease infinite;
+    animation: gradientAnimation 20s ease infinite;
   }
 </style>
